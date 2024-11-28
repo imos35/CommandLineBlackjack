36766cde673e39951cc6dea697a05dcd029f8f75
Willhelm Gustavsson <ycqezdcp@students.codeinstitute.net>
2024-11-28 21:40:53 +0000

    added game logic for player and dealers turn

--- run.py
+++ run.py
@@ -13,3 +13,42 @@ def deal_cards(deck):
     Deals a card
     """
     return deck.pop()
+
+
+
+def shuffle_and_deal():
+    """
+    Shuffles and deals players their hands. (Starts game)
+    """
+    deck = create_deck()
+    random.shuffle(deck)
+
+    players_hand = [deal_cards(deck), deal_cards(deck)]
+    dealers_hand = [deal_cards(deck), deal_cards(deck)]
+
+def calculate_hand_value(hand):
+    total = sum(card[0] for card in hand)
+    for card in hand:
+        if total > 21 and card[0] == 1:
+            total -= 10
+    return total
+
+while calculate_hand_value(players_hand) < 21:
+    print(f'Your hand: {player_hand}, -{calculate_hand_value(player_hand)}-')
+
+    hit_or_stand = input('Do you want to Hit or Stand?').lower()
+
+    if hit_or_stand == 'hit':
+        player_hand.append(deal_cards(deck))
+        break
+    elif hit_or_stand == 'stand':
+        break
+    else:
+        print("Invalid input! Please type 'Hit' or 'Stand'.")
+
+while calculate_hand_value(dealers_hand) < 17:
+    dealers_hand.append(deal_cards(deck))
+
+
+
+shuffle_and_deal()
