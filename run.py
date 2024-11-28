3385fbe8b1950ac9cce08eb37fe463167bb3f531
Willhelm Gustavsson <ycqezdcp@students.codeinstitute.net>
2024-11-28 21:01:43 +0000

    Created deck and function to deal cards

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def create_deck():
+    """
+    Creates the deck and the cards values
+    """
+    card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    return [(value, suit) for value in values for suit in suits]
+
+def deal_cards(deck):
+    """
+    Deals a card
+    """
+    return deck.pop()
